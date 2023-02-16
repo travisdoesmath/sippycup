04a6e5395f44a132f583e493f934519629944f49
Travis Taylor <travisdoesmath@gmail.com>
2023-02-16 12:08:17 -0600

    fixed routing bug

--- frontend/app.py
+++ frontend/app.py
@@ -6,8 +6,9 @@ app = Flask(__name__, static_url_path='', static_folder='build')
 @app.route("/", defaults={'id':''})
 @app.route("/<string:id>")
 def load(id):
+    print(id)
     def is_valid_id(id):
-        return bool(re.match(r'[a-z]+-[a-z]+-[a-z]+', 'a-a-'))
+        return bool(re.match(r'[a-z]+-[a-z]+-[a-z]+', id))
 
     if (id == '' or is_valid_id(id)):
         return send_from_directory(app.static_folder, 'index.html')
